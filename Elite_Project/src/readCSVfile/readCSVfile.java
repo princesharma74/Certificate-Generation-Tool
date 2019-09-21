b8dcca057b92d7b62c1994843bdccbb3c40d72e4
Prince Sharma <Prince Sharma@192.168.0.100>
2019-09-21 08:09:54 +0530

    updated

--- Elite_Project/src/readCSVfile/readCSVfile.java
+++ Elite_Project/src/readCSVfile/readCSVfile.java
@@ -1,35 +1,37 @@
 package readCSVfile;
 
 import java.io.*;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileSystemView;
+//import javax.swing.JFileChooser;
+//import javax.swing.filechooser.FileSystemView;
 import org.apache.commons.csv.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 public class readCSVfile
 {
     private static String path = "";
-    public static void main(String[] args) throws IOException{                      
+   public static void main(String[] args) throws IOException{           
+	   /*
         JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
         int returnValue = jfc.showOpenDialog(null);
         if (returnValue == JFileChooser.APPROVE_OPTION){
             File selectedFile = jfc.getSelectedFile();  
             path =  selectedFile.getAbsolutePath();
+            System.out.println(path);
             
-        }
+        }*/
+	   path = "C:/Users/Prince Sharma/Desktop/hey.csv";
+	   
         try(
             Reader reader = Files.newBufferedReader(Paths.get(path));
             CSVParser CSVParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader().withSkipHeaderRecord().withIgnoreHeaderCase().withTrim());
-        ){
+        ){        	
+        	//System.out.println("Lasers");
             for(CSVRecord CSVRecord : CSVParser){
-                String Event = CSVRecord.get("Event");
-                String Year = CSVRecord.get("Year");
-                String Name = CSVRecord.get("Name");
-                String Position = CSVRecord.get("Position");                
-                String Course = CSVRecord.get("Course");
-                System.out.println("This certificate is awarded to "+Name+" of "+Course+" for securing "+Position+" in the "+Event+" of ANDC FEST "+ Year+".\n");
-            }
+            	String Email = CSVRecord.get("Email1");
+            		System.out.println(Email);
+            	}          	
+              }
         }
-    }    
-    
-}
+    }
+
+
